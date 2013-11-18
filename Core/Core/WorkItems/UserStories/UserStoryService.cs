856274a390523fd22f33e47d8650363501cfa2ee
victor <vrusu@fareportal.com>
2013-11-18 16:38:58 -0500

    user story app service

---
+++ Core/Core/WorkItems/UserStories/UserStoryService.cs
@@ -0,0 +1,11 @@
+﻿using Messages.UserStory;
+
+namespace Core.WorkItems.UserStories
+{
+    public class UserStoryService: IUserStoryApplicationService
+    {
+        public void When(CreateUserStory createUserStory)
+        {
+        }
+    }
+}
