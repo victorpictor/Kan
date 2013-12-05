4169832ef39e7f1777f16ab7ca3c077b4442b6c6
victor <vrusu@fareportal.com>
2013-12-05 14:58:34 -0500

    renamed spec file

---
+++ Specs/Core.Specs/UserStoryScenarios/Service/IWantToCreateUserStory/SubmitingInvalidCommand.cs
@@ -0,0 +1,64 @@
+﻿using Messages.Identities;
+using Messages.UserStory;
+using NUnit.Framework;
+
+namespace Core.Specs.UserStoryScenarios.Service.IWantToCreateUserStory
+{
+
+    [TestFixture]
+    public class WithNullCommand : UserStoryServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(NullCommandException), ExpectedMessage = "Create user story command was null")]
+        public void ItShouldThrowNullComandException()
+        {
+            service.When((CreateUserStory)null);
+        }
+    }
+
+    [TestFixture]
+    public class WithEmptyUserStoryName : UserStoryServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(StringValueException), ExpectedMessage = "User story name was expected to be text")]
+        public void ItShouldThrowStringValueException()
+        {
+            service.When(new CreateUserStory(){Name = ""});
+        }
+    }
+
+    [TestFixture]
+    public class WithNullUserStoryName : UserStoryServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(StringValueException), ExpectedMessage = "User story name was expected to be text")]
+        public void ItShouldThrowStringValueException()
+        {
+            service.When(new CreateUserStory() { Name = null });
+        }
+    }
+
+
+    [TestFixture]
+    public class WithEmptyUserStoryDescription : UserStoryServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(StringValueException), ExpectedMessage = "User story description was expected to be text")]
+        public void ItShouldThrowStringValueException()
+        {
+            service.When(new CreateUserStory() { Name = "I want", Description = "" });
+        }
+    }
+
+    [TestFixture]
+    public class WithNullUserStoryDescription : UserStoryServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(StringValueException), ExpectedMessage = "User story description was expected to be text")]
+        public void ItShouldThrowStringValueException()
+        {
+            service.When(new CreateUserStory() { Name = "I want", Description = null });
+        }
+    }
+
+}
