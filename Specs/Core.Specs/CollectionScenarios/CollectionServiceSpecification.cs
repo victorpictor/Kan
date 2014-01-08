c1c90edbd5d8ec2ff898e3950edece381268efde
victor <vrusu@fareportal.com>
2014-01-08 14:15:53 -0500

    corrected names in specification

---
+++ Specs/Core.Specs/CollectionScenarios/CollectionServiceSpecification.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Core.Board.Collections;
+using Core.WorkItems.UserStories;
+using Messages;
+using Messages.Identities;
+using Messages.Markers;
+using Moq;
+using NUnit.Framework;
+
+namespace Core.Specs.CollectionScenarios
+{
+    [TestFixture]
+    public class CollectionServiceSpecification: Specification
+    {
+        protected CollectionService service;
+
+        protected Mock<IEventStore> eventStore;
+        protected Mock<IPublishEvents> eventsPublisher;
+
+        protected CollectionIdentity Identity;
+
+        protected override void Given()
+        {
+            Identity = new CollectionIdentity(1);
+
+            eventStore = new Mock<IEventStore>();
+            eventStore.Setup(es => es.GetStream(Identity)).Returns(new List<IEvent>());
+
+            eventsPublisher = new Mock<IPublishEvents>();
+
+            service = new CollectionService(eventStore.Object, eventsPublisher.Object);
+        }
+    }
+}
---
+++ Specs/Core.Specs/CollectionScenarios/Service/IWantToCreateCollection/SubmitingInvalidCommand.cs
@@ -0,0 +1,52 @@
+﻿using Messages.Collection.Commands;
+using Messages.Identities;
+using Messages.UserStory;
+using NUnit.Framework;
+
+namespace Core.Specs.CollectionScenarios.Service.IWantToCreateCollection
+{
+    [TestFixture]
+    public class WithNullCommand : CollectionServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(NullCommandException), ExpectedMessage = "Create collection command was null")]
+        public void ItShouldThrowNullComandException()
+        {
+            service.When((CreateCollection)null);
+        }
+    }
+
+
+    [TestFixture]
+    public class WithNullName : CollectionServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(StringValueException), ExpectedMessage = "Collection name was null or empty")]
+        public void ItShouldThrowStringValueException()
+        {
+            service.When(new CreateCollection(){Name = null});
+        }
+    }
+
+    [TestFixture]
+    public class WithEmptyName : CollectionServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(StringValueException), ExpectedMessage = "Collection name was null or empty")]
+        public void ItShouldThrowStringValueException()
+        {
+            service.When(new CreateCollection() { Name = string.Empty });
+        }
+    }
+
+    [TestFixture]
+    public class WithNegativeWipLimit : CollectionServiceSpecification
+    {
+        [Test]
+        [ExpectedException(typeof(IntValueException), ExpectedMessage = "Wip value was less than 0")]
+        public void ItShouldThrowIntValueException()
+        {
+            service.When(new CreateCollection() { Name = "Wip", WipLimit = -1});
+        }
+    }
+}
