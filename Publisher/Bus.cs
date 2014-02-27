09628a5782edee52e1af6b35fffd8f54280e7526
victor <vrusu@fareportal.com>
2014-02-27 11:16:36 -0500

    publisher

---
+++ Publisher/Bus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Core;
+using Messages.Markers;
+
+namespace Publisher
+{
+    public class Bus : IPublishEvents
+    {
+        public void Publish(IEvent e)
+        {
+        }
+
+        public void Publish(List<IEvent> es)
+        {
+        }
+    }
+}
